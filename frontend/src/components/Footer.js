e007106c53b56bec1f4cb4e5f1cef1e0178dfa32
EagleTopOFC <116709074+EagleTopOFC@users.noreply.github.com>
2024-12-18 18:44:25 -0800

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Panel SMM</p>
+    </footer>
+  );
+}
+
+export default Footer;
