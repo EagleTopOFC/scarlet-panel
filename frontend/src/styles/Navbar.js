f28c2d6ffc5adbd2734e93071230b1a7ac5932ed
EagleTopOFC <116709074+EagleTopOFC@users.noreply.github.com>
2024-12-18 18:48:04 -0800

    Add files via upload

---
+++ frontend/src/styles/Navbar.js
@@ -0,0 +1,24 @@
+.navbar {
+  background-color: #333;
+  padding: 10px;
+  color: white;
+}
+
+.navbar ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.navbar li {
+  display: inline;
+  margin-right: 20px;
+}
+
+.navbar a {
+  color: white;
+  text-decoration: none;
+}
+
+.navbar a:hover {
+  text-decoration: underline;
+}
